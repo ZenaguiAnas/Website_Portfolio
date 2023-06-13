944b1eb75742c3479514c2c17e067766a910b303
Anas Zenagui <zenaguianas20@gmail.com>
2023-06-13 20:42:21 +0100

    refactoring the code

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = () => {
               <Link href="/#projects">Projects</Link>
             </li>
             <li className="ml-10 text-sm uppercase hover:border-b">
-              <Link href="/resume">Resume</Link>
+              <Link href="https://github.com/ZenaguiAnas">Resume</Link>
             </li>
             <li className="ml-10 text-sm uppercase hover:border-b">
               <Link href="/#contact">Contact</Link>
