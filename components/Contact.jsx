f533de89f3487e460d197ce0e6958719233edc9d
Anas Zenagui <zenaguianas20@gmail.com>
2024-06-27 02:51:34 +0100

    Handling the contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
@@ -75,7 +75,7 @@ const Contact = () => {
           <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
             <div className='p-4'>
               <form
-                action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
+                action='https://getform.io/f/lbkmmgdb'
                 method='POST'
                 encType='multipart/form-data'
               >
