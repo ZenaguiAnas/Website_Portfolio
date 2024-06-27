cb64a4913987d3d5d6108a4e5d4ff0f723cdc722
Anas Zenagui <zenaguianas20@gmail.com>
2024-06-27 15:57:47 +0100

    Add the script of Microsoft clarity

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,26 @@
-import Navbar from '../components/Navbar';
-import '../styles/globals.css';
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "myjmydr234");`,
+          }}
+        />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </>
   );
 }
-
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,16 @@ export default function Document() {
         `,
           }}
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "myjmydr234");`,
+          }}
+        />
       </Head>
       <body>
         <Main />
